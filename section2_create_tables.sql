97c7712816a2e3842ad26e28f92b1ea3aafa0189
Angela Quek <angela.smquek@gmail.com>
2022-09-18 00:57:44 +0800

    added Manufacter Model

--- section2_create_tables.sql
+++ section2_create_tables.sql
@@ -1,10 +1,18 @@
-CREATE TABLE IF NOT EXISTS "CarModel"
+CREATE TABLE IF NOT EXISTS "Car"
 (
-    car_serial_id serial PRIMARY KEY,
+    car_serial_id serial NOT NULL,
+    car_weight smallint NOT NULL,
+    price money NOT NULL,
+    CONSTRAINT "car_serial_PK" PRIMARY KEY car_serial_id,
+    CONSTRAINT "car_model_FK" FOREIGN KEY ("manufac_model_id")
+        REFERENCES "ManufacturerCarModel" ("manufac_model_id")
+);
+
+CREATE TABLE IF NOT EXISTS "ManufacturerCarModel"
+(
+    manufac_model_id serial PRIMARY KEY,
     model_name varchar(255) NOT NULL,
-    manufac_name varchar(255) NOT NULL,
-    car_weight smallint,
-    price money NOT NULL
+    manufac_name varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Customer"
@@ -33,21 +41,3 @@ CREATE TABLE IF NOT EXISTS "CarSales"
         REFERENCES "CarModel" ("car_serial_id")
 );
 
-user_id serial PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-        last_login TIMESTAMP 
-
-
-        CREATE TABLE account_roles (
-  user_id INT NOT NULL,
-  role_id INT NOT NULL,
-  grant_date TIMESTAMP,
-  PRIMARY KEY (user_id, role_id),
-  FOREIGN KEY (role_id)
-      REFERENCES roles (role_id),
-  FOREIGN KEY (user_id)
-      REFERENCES accounts (user_id)
-);
