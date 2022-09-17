ec94e908cfd7140113dc807d242ad673f7885197
Angela Quek <angela.smquek@gmail.com>
2022-09-18 01:57:58 +0800

    updated FK

--- section2_carsales.sql
+++ section2_carsales.sql
@@ -1,17 +1,17 @@
 --list of customers and their spending in descending order
-select cu.cust_id as "customer id", cu.cust_name as "customer name", sum(c.price) as "spending",
-from CarSales as cs 
-join Car as c on cs.car_serial_id = c.car_serial_id
-join Customer as cu on cs.cust_id = cu.cust_id
+select cu.cust_id as "customer id", cu."cust_name" as "customer name", sum(c.price) as "spending"
+from public."CarSales" as cs 
+join public."Car" as c on cs."trans_id" = c."trans_id"
+join public."Customer" as cu on cs."cust_id" = cu."cust_id"
 group by cu.cust_id, cu.cust_name
 order by sum(c.price) desc;
 
 
 --top 3 manufacturers with sales quantity for the current month
 select cm.manufac_model_id, cm.manufac_name as "manufacturer", count(cs.trans_id) as "sales quantity"
-from CarSales as cs 
-join Car as c on cs.car_serial_id = c.car_serial_id
-join ManufacturerCarModel as cm on c.manufac_model_id = cm.manufac_model_id
+from public."CarSales" as cs 
+join public."Car" as c on cs."trans_id" = c."trans_id"
+join public."ManufacturerCarModel" as cm on c."manufac_model_id" = cm."manufac_model_id"
 where date_trunc('month', cs.trans_datetime)= date_trunc('month', CURRENT_DATE);
 group by cm.manufac_model_id, cm.manufac_name
 order by count(cs.trans_id) desc
--- section2_create_tables.sql
+++ section2_create_tables.sql
@@ -1,43 +1,46 @@
-CREATE TABLE IF NOT EXISTS "Car"
-(
-    car_serial_id serial NOT NULL,
-    car_weight smallint NOT NULL,
-    price money NOT NULL,
-    CONSTRAINT "car_serial_PK" PRIMARY KEY car_serial_id,
-    CONSTRAINT "car_model_FK" FOREIGN KEY ("manufac_model_id")
-        REFERENCES "ManufacturerCarModel" ("manufac_model_id")
-);
-
-CREATE TABLE IF NOT EXISTS "ManufacturerCarModel"
-(
-    manufac_model_id serial PRIMARY KEY,
-    model_name varchar(255) NOT NULL,
-    manufac_name varchar(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS "Customer"
+--DDL statements to create tables based on ERD
+CREATE TABLE IF NOT EXISTS public."Customer"
 (
     cust_id serial PRIMARY KEY,
     cust_name varchar(255) NOT NULL,
     phone_no varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "SalesPerson"
+CREATE TABLE IF NOT EXISTS public."SalesPerson"
 (
     sales_per_id serial PRIMARY KEY,
     sales_per_name varchar(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "CarSales"
+CREATE TABLE IF NOT EXISTS public."ManufacturerCarModel"
 (
-    trans_id serial NOT NULL,
+    manufac_model_id serial PRIMARY KEY,
+    model_name varchar(255) NOT NULL,
+    manufac_name varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public."CarSales"
+(
+    trans_id serial PRIMARY KEY,
     trans_datetime TIMESTAMP NOT NULL,
-    CONSTRAINT "trans_PK" PRIMARY KEY trans_id,
+    cust_id integer NOT NULL,
+    sales_per_id integer NOT NULL,
     CONSTRAINT "trans_cust_FK" FOREIGN KEY ("cust_id")
-        REFERENCES "Customer" ("cust_id"),
+        REFERENCES public."Customer" ("cust_id"),
     CONSTRAINT "trans_salesper_FK" FOREIGN KEY ("sales_per_id")
-        REFERENCES "SalesPerson" ("sales_per_id"),
-    CONSTRAINT "trans_car_serial_FK" FOREIGN KEY ("car_serial_id")
-        REFERENCES "CarModel" ("car_serial_id")
+        REFERENCES public."SalesPerson" ("sales_per_id")
+);
+
+CREATE TABLE IF NOT EXISTS public."Car"
+(
+    car_serial_id serial PRIMARY KEY,
+    car_weight smallint NOT NULL,
+    price money NOT NULL,
+    manufac_model_id integer NOT NULL,
+    trans_id integer NOT NULL,
+    CONSTRAINT "car_model_FK" FOREIGN KEY ("manufac_model_id")
+        REFERENCES public."ManufacturerCarModel" ("manufac_model_id"),
+    CONSTRAINT "car_trans_FK" FOREIGN KEY ("trans_id")
+        REFERENCES public."CarSales" ("trans_id")
 );
 
